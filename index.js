25b2cfafe2ddb856bfeda7799cdde88a6f184c77
romerojoseing <romerojoseing@gmail.com>
2020-04-20 18:51:10 -0400

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// El protocolo HTTP consiste en recibir peticiones de los clientes y dar respuestas desde el servidor 
+/* El protocolo HTTP consiste en recibir peticiones de los clientes y dar respuestas desde el servidor 
 const http = require('http');
 const colors = require('colors');
 
@@ -13,4 +13,17 @@ const handleServer = function(req, res){
 const server = http.createServer(handleServer);                    
 server.listen(3000, ()=>{                       // Escuchando desde el puerto 3000
     console.log("Server on Port 3000".cyan);    // Mostrar por consola
-});                          
+});*/                          
+
+const express = require('express');
+const colors = require('colors');
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('<h1>Aqui Node.js y Espress</h1>');
+    res.end();
+});
+server.listen(3000, () => {
+    console.log("Server on Port 3000".cyan);
+});
